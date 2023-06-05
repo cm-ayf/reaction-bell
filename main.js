917207adc98d5bbee62559033c9b17f84cf99b0b
cm-ayf <cm.ayf2734@gmail.com>
2023-06-05 16:19:41 +0900

    init

---
+++ main.js
@@ -0,0 +1,35 @@
+// @ts-check
+import { Client, GatewayIntentBits, Partials, Events } from "discord.js";
+import { setTimeout } from "timers/promises";
+
+const EMOJI = "🔔";
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessageReactions],
+  partials: [Partials.Message, Partials.Reaction],
+});
+
+client.on(Events.ClientReady, (client) =>
+  console.log(`Logged in as ${client.user.tag}`)
+);
+
+client.on(Events.MessageReactionAdd, async (reaction, user) => {
+  if (reaction.emoji.name === EMOJI) return;
+
+  const message = await reaction.message.fetch();
+  const bells = message.reactions.cache.get(EMOJI);
+  if (!bells) return;
+
+  /** @type {import("discord.js").MessageCreateOptions} */
+  const options = {
+    content: `\
+    ${reaction.users.cache.toJSON().join(" ")}
+    **${user.tag}** reacted to your message with ${reaction.emoji}`,
+    allowedMentions: { repliedUser: false },
+  };
+  for (const user of bells.users.cache.values()) {
+    await Promise.race([user.send(options), setTimeout(1000)]);
+  }
+});
+
+client.login();
